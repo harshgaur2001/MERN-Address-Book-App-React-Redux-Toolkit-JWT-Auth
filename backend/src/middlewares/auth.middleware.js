40aea9dd5cdb418c28a67ce95fde777686b29259
Harsh Gaur <harshgaur040@hmail.com>
2025-11-20 23:26:54 +0530

    Final Commit

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   const token =
     req.cookies?.accessToken ||
-    req.header("Autorization")?.replace("Bearer ", "");
+    req.header("Authorization")?.replace("Bearer ", "");
 
   if (!token) {
     throw new ApiError(401, "Unauthorized request");
